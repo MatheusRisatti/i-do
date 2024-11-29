35f49cbbad7f3ba5149ed13918662e25417ce125
MatheusRisatti <risattimatheus@gmail.com>
2024-11-29 15:05:44 -0300

    docs: altera o local no qual os componentes ficam armazenados

--- src/pages/app/template1/components/footer/index.tsx
+++
@@ -1,9 +0,0 @@
-export function Footer() {
-  return (
-    <>
-      <div className="flex h-20 w-full items-center justify-center bg-[#faf0e4]">
-        <h3>Feito com carinho por Mpk & Zz</h3>
-      </div>
-    </>
-  )
-}
